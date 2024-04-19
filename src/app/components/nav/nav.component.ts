81f9fba50243f57b7f5cba13a9c21b15bcf912f0
johannesstroebele91 <stroebele.j@gmail.com>
2024-04-19 07:49:06 +0200

    Setup navigation

---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.scss']
+})
+export class NavComponent {
+
+    showMobileMenu = false;
+
+    showMenuMobile(): boolean {
+        return this.showMobileMenu = !this.showMobileMenu;
+    }
+
+    closeNav() {
+        this.showMobileMenu = false;
+    }
+}
