17d2b66a939c2718f455b8e0b5e20428b652adb7
johannesstroebele91 <stroebele.j@gmail.com>
2024-04-21 14:48:18 +0200

    Automatically set current year for footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,16 +3,17 @@ import {ViewportScroller} from '@angular/common';
 import {Router} from '@angular/router';
 
 @Component({
-    selector: 'app-footer',
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.scss']
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  currentYear: number = new Date().getFullYear();
 
-    constructor(private viewportScroller: ViewportScroller, public router: Router) {
-    }
+  constructor(private viewportScroller: ViewportScroller, public router: Router) {
+  }
 
-    public onClick(elementId: string): void {
-        this.viewportScroller.scrollToAnchor(elementId);
-    }
+  public onClick(elementId: string): void {
+    this.viewportScroller.scrollToAnchor(elementId);
+  }
 }
