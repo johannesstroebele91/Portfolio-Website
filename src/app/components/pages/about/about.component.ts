ed333ad65580f5c4216a8be93ac9a62ab26c7e77
johannesstroebele91 <stroebele.j@gmail.com>
2024-04-23 14:02:06 +0200

    Improve about page and remove skills from about page

--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -4,42 +4,33 @@ import {ActivatedRoute} from '@angular/router';
 import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
-    selector: 'app-about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.scss']
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
 
-    showContentFrontend = false;
-    showContentBackend = false;
-    showContentUxDesign = false;
-    showContentDSResearch = false;
+  constructor(private viewportScroller: ViewportScroller, private route: ActivatedRoute, private title: Title, private meta: Meta) {
+    this.title.setTitle('About page');
+    this.meta.addTags([
+      {
+        name: 'author',
+        content: 'Johannes Ströbele'
+      },
+      {
+        name: 'description',
+        content: 'My goal is to develop business applications that optimize the user experience and business value.' +
+          'On this page, you can view my skills, working experience, and studies.'
+      },
+      {
+        name: 'keywords',
+        content: 'Johannes Ströbele, Johannes Stroebele, portfolio, about, full stack developer, software developer,' +
+          'information design, front end development, back end development, business application'
+      },
+    ], true);
+  }
 
-    constructor(private viewportScroller: ViewportScroller, private route: ActivatedRoute, private title: Title, private meta: Meta) {
-        this.title.setTitle('About page');
-        this.meta.addTags([
-            {
-                name: 'author',
-                content: 'Johannes Ströbele'
-            },
-            {
-                name: 'description',
-                content: 'My goal is to develop business applications that optimize the user experience and business value.' +
-                    'On this page, you can view my skills, working experience, and studies.'
-            },
-            {
-                name: 'keywords',
-                content: 'Johannes Ströbele, Johannes Stroebele, portfolio, about, full stack developer, software developer,' +
-                    'information design, front end development, back end development, business application'
-            },
-        ], true);
-    }
-
-    public onClick(elementId: string): void {
-        this.viewportScroller.scrollToAnchor(elementId);
-    }
-
-    openFile(filename: string) {
-        window.open('../../assets/' + filename);
-    }
+  public onClick(elementId: string): void {
+    this.viewportScroller.scrollToAnchor(elementId);
+  }
 }
