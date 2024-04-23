b1d783174ea34e28e7aab0bd8a80f517b4e6d156
johannesstroebele91 <stroebele.j@gmail.com>
2024-04-23 14:49:58 +0200

    Improve home page

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,36 +1,44 @@
 import {Component} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {Router} from '@angular/router';
 import {Meta, Title} from '@angular/platform-browser';
+import {ViewportScroller} from '@angular/common';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 
 
-    constructor(private route: ActivatedRoute, private title: Title, private meta: Meta) {
-        this.title.setTitle('Johannes Ströbele');
-        this.meta.addTags([
-            {
-                name: 'author',
-                content: 'Johannes Ströbele'
-            },
-            {
-                name: 'description',
-                content: 'Welcome! I am Johannes, a full stack cloud developer. On my homepage you can view my two most recent projects, ' +
-                    'the companies I worked with, and what others say about me.'
-            },
-            {
-                name: 'keywords',
-                content: 'Johannes Ströbele, Johannes Stroebele, portfolio, homepage, full stack developer, software developer,' +
-                    'information design, front end development, back end development, business application'
-            },
-        ], true);
-    }
+  constructor(private router: Router, private title: Title, private meta: Meta, private viewportScroller: ViewportScroller) {
+    this.title.setTitle('Johannes Ströbele');
+    this.meta.addTags([
+      {
+        name: 'author',
+        content: 'Johannes Ströbele'
+      },
+      {
+        name: 'description',
+        content: 'Welcome! I am Johannes, a full stack cloud developer. On my homepage you can view my two most recent projects, ' +
+          'the companies I worked with, and what others say about me.'
+      },
+      {
+        name: 'keywords',
+        content: 'Johannes Ströbele, Johannes Stroebele, portfolio, homepage, full stack developer, software developer,' +
+          'information design, front end development, back end development, business application'
+      },
+    ], true);
+  }
 
-    openWebsite(url: string) {
-        window.location.href = url;
-    }
+  openWebsite(url: string) {
+    window.location.href = url;
+  }
+
+  public navToAboutPageCompanySection(): void {
+    this.router.navigate(['/about']).then(() => {
+      this.viewportScroller.scrollToAnchor('company');
+    });
+  }
+
 }
